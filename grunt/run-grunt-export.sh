381ea5bd301bd137965ffab1a3a23a733485c794
Jantzen Ward <jward@degdigital.com>
2017-11-09 14:43:17 -0600

    Fix permission error when creating directory

--- grunt/run-grunt-export.sh
+++ grunt/run-grunt-export.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir -p /var/www/.npm
 chown www-data:www-data /var/www/.npm/
+mkdir -p /var/www/.jspm
+chown www-data:www-data /var/www/.jspm/
 cd /var/www/magento/ui
 su -s /bin/sh www-data -c "/usr/bin/npm install"
 su -s /bin/sh www-data -c "/usr/bin/jspm install -y"
