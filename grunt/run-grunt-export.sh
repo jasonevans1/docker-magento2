7b08cd035f66706edc8872e99665b0d0909823d5
Jantzen Ward <jward@degdigital.com>
2017-11-09 14:21:49 -0600

    Fix grunt export command not running jspm install

--- grunt/run-grunt-export.sh
+++ grunt/run-grunt-export.sh
@@ -3,4 +3,5 @@ mkdir -p /var/www/.npm
 chown www-data:www-data /var/www/.npm/
 cd /var/www/magento/ui
 su -s /bin/sh www-data -c "/usr/bin/npm install"
+su -s /bin/sh www-data -c "/usr/bin/jspm install -y"
 su -s /bin/sh www-data -c "/usr/bin/grunt export"
